bde4c4f1539b2f58e6b33ab64a26eba6ac57ebe8
klausdw <klausdw@outlook.com>
2020-12-16 15:07:59 +0100

    state, setState

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
+class App extends Component {
+
+  constructor() {
+    super();
+
+    this.state = {
+      string: 'Hallo Klaus Dantas Wagner'
+    };
+  }
+
+
+  render() {
+    return (
+      <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p> { this.state.string } </p>
+        <button onClick={ () => this.setState( { string:'Hallo Klaus' } ) }>Change Text</button>
       </header>
     </div>
-  );
+    )
+  }
 }
 
 export default App;
