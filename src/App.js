22456f9fd1dd525c6a1939653d5dfda5eeb9ca85
klausdw <klausdw@outlook.com>
2020-12-17 13:46:59 +0100

    props

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+
+import { CardList } from './components/card-list/card-list.jsx';
+
 import './App.css';
 
 class App extends Component {
@@ -12,13 +15,16 @@ class App extends Component {
 
   componentDidMount() {
     fetch('https://jsonplaceholder.typicode.com/users')
-    .then(response => response.json())
-    .then(users => this.setState({ mosnters: users }));
+      .then(response => response.json())
+      .then(users => this.setState({ mosnters: users }));
   }
 
   render() {
     return (
       <div className="App">
+        <CardList name='Klaus'>
+          <h1>Klaus</h1>
+        </CardList>
         {
           this.state.mosnters.map(monster => <h1 key={monster.id}> {monster.name} </h1>)
         }
