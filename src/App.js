ea4f2bc882e7a367661f5c92abb62e01895489ab
klausdw <klausdw@outlook.com>
2020-12-17 17:08:33 +0100

    css

--- src/App.js
+++ src/App.js
@@ -32,6 +32,7 @@ class App extends Component {
 
     return (
       <div className="App">
+        <h1>Monster Rolodex</h1>
         <SearchBox 
           placeholder='search monsters'
           handleChange={this.handleChange}
